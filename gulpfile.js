eca76b0f4d8d54f67155b29bd11c5744e767770d
Jens Buechner <jb@civilix.de>
2017-05-19 18:23:35 +0200

    added gulp task to copy resources to dist when building.

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,29 @@ const $del = require('del');
 
 const project = {
 	paths: {
-		dist: './dist'
+		dist: {
+			root: './dist',
+			resources: './dist/resources'
+		},
+		src: {
+			resources: './src/resources/**/*'
+		}
 	}
 }
 
 $gulp.task('default', ['rebuild']);
 
-$gulp.task('rebuild', ['clean']);
+$gulp.task('rebuild', ['clean', 'build']);
+
+$gulp.task('build', ['copy']);
+
+$gulp.task('copy', ['copy-resources']);
 
 $gulp.task('clean', function() {
-	return $del([project.paths.dist]);
+	return $del([project.paths.dist.root]);
+});
+
+$gulp.task('copy-resources', function() {
+	return $gulp.src(project.paths.src.resources)
+		.pipe($gulp.dest(project.paths.dist.resources));
 });
